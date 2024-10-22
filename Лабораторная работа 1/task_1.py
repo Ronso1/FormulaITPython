1736e6c540f122053845bb659559c66972dd527e
Ivan <gureff.al@yandex.ru>
2024-10-22 22:45:32 +0300

    Rename file name

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,8 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-index_of_none = 4
 
-new_numbers = [2, -93, -2, 8, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-new_number = sum(new_numbers) / len(numbers)
-numbers[index_of_none] = new_number
+index = 4
 
+sum_numbers = sum(numbers[:index]) + sum(numbers[index + 1:])
+cnt_numbers = len(numbers)
+average_of_numbers = sum_numbers / cnt_numbers
+
+numbers[index] = average_of_numbers
 print("Измененный список:", numbers)
