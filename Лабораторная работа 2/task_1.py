26acf46469fa7b3255387f3ba6e77646edd76d7c
Ivan <gureff.al@yandex.ru>
2024-10-23 22:16:47 +0300

    Upload tasks for lab 2

---
+++ Лабораторная работа 2/task_1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+count_month = 0
+# Строки 8 и 9 рассчитывают первый месяц без начисления процента на расходы
+money_capital += salary
+money_capital -= spend
+
+while money_capital > 0:
+    money_capital += salary
+    spend = spend * (1 + increase)
+    money_capital -= spend
+    count_month += 1
+
+print("Количество месяцев, которое можно протянуть без долгов:", count_month)
